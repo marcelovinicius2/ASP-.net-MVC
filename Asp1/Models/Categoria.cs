1c628e5e9994aa159cc64d83693592cb57ad3333
marcelovinicius2 <marcelovinicius2@hotmail.com>
2021-08-11 14:44:15 -0300

    commit ajuste 1

---
+++ Asp1/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp1.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
+ 
---
+++ Asp1/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp1.Models
+{
+    public class Context : DbContext
+       
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         {
+            optionsBuilder.UseSqlServer(@"Server=(localbd)\mssqllocalbd;Database=Cursomvc;Integrated Security=True");
+         }
+    }
+
+}
